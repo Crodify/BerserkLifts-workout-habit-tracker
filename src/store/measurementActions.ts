0064d6ae2ac0031286aa90f0e6d541c450bc8fd2
Ranveer Singh Bhatia <66438108+Crodify@users.noreply.github.com>
2026-08-30 13:06:58 +0530

    Rebuild with Supabase sync fixes

--- src/store/measurementActions.ts
+++
@@ -1,9 +0,0 @@
-import { BodyMeasurement } from '@/types';
-
-export const measurementActions = (set: any, get: any) => ({
-  addBodyMeasurement: (measurement: BodyMeasurement) => {
-    set((state: any) => ({
-      bodyMeasurements: [measurement, ...state.bodyMeasurements],
-    }));
-  },
-});
